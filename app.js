163f91c230cbd230b7b36c8ace25261aa1f95e56
charan <charan9487@gmail.com>
2020-06-07 12:43:35 +0530

    second commit

--- app.js
+++ app.js
@@ -27,104 +27,12 @@ const {
 
 app.use(express.static('public'));
 
-//let count=0;
-/*
-let greeting="welcome";
-
-
-io.on('connection',function(socket){
-	console.log("New user connected");
-
-
-
-	socket.on('join',(options,callback)=>{
-		//adding users
-
-		const {error,user}=addUser({id:socket.id,...options})
-
-		if(error){
-			return callback(error)
-
-		}
-		socket.join(user.room)
-
-		//Sending msgs from server to client
-
-		socket.emit("greeting",generateMessage(greeting))
-
-	//Sending msg to all except new user
-
-	socket.broadcast.to(user.room).emit("greeting",generateMessage(`${user.username} has joined`))
-
-	callback()
-
-})
-
-
-
-    //Receiving information from client
-
-    socket.on('user',(msg,callback)=>{
-
-    	const user=getUser(socket.id)
-
-        //Filtering foul words
-
-        const filter=new Filter()
-
-
-        if(filter.isProfane(msg)){
-        	return callback('Profanity is not allowed')
-        }
-        
-        //Sending msg from server to client 
-
-        io.to(user.room).emit("greeting",generateMessage(msg))
-        callback()
-
-
-    })
-
-    //Receiving location from client
-
-    socket.on("sendLocation",(coords,callback)=>{
-
-    	const user=getUser(socket.id)
-
-
-    	io.to(user.room).emit("location",generateLocation(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
-    	callback()
-    })
-    
-    //Disconnect
-    socket.on('disconnect', () => {
-    	const user=removeUser(socket.id)
-    	if(user){
-    		io.to(user.room).emit("greeting",generateMessage(`${user.username} has left`))
-
-    	}
-    	
-
-    })
-
-})
-
-
-
-
-
-
-server.listen(3000,function(){
-	console.log("server started");
-})
-*/
-
 io.on('connection', (socket) => {
 	console.log('New WebSocket connection')
 
 	socket.on('join', (options, callback) => {
 		const { error, user } = addUser({ id: socket.id, ...options })
-		console.log('user',user)
+		
 		if (error) {
 			return callback(error)
 		}
@@ -172,6 +80,7 @@ io.on('connection', (socket) => {
 	})
 })
 
-server.listen(3000, () => {
-	console.log(`Server is up on port!`)
-})
+server.listen(process.env.PORT || 3000
+	, () => {
+		console.log(`Server is up on port!`)
+	})
